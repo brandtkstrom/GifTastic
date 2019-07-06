37550732948a31a5fdcad70000c1eea544a687f6
Brandt Strom <brandtkstrom@gmail.com>
2019-07-06 14:33:33 -0700

    update giphy api base url

--- assets/scripts/constants.js
+++ assets/scripts/constants.js
@@ -1,31 +1,31 @@
-const API_BASE_URI = "https://api.giphy.com/v1/gifs/search";
+const API_BASE_URI = '//api.giphy.com/v1/gifs/search';
 
-const API_KEY = "p8D59yovNeP3PTeEY050e6xzDnfjuMhu";
+const API_KEY = 'p8D59yovNeP3PTeEY050e6xzDnfjuMhu';
 
 const GIF_LIMIT = 10;
 
 const DEFAULT_PARAMS = [
-    "rating=pg",
-    "lang=en",
+    'rating=pg',
+    'lang=en',
     `limit=${GIF_LIMIT}`,
     `api_key=${API_KEY}`
 ];
 
-const FAV_KEY = "gif-favorite";
+const FAV_KEY = 'gif-favorite';
 
 const GIF_STATE = {
-    ANIMATED: "animated",
-    STATIC: "static"
+    ANIMATED: 'animated',
+    STATIC: 'static'
 };
 
 const TOPICS = [
-    "burger",
-    "pizza",
-    "cheese",
-    "beef",
-    "bacon",
-    "cookie",
-    "candy",
-    "beer",
-    "sushi"
+    'burger',
+    'pizza',
+    'cheese',
+    'beef',
+    'bacon',
+    'cookie',
+    'candy',
+    'beer',
+    'sushi'
 ];
